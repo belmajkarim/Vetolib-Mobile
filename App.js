787d15225a5df68dc4029f6447e7ba4b0ba9719b
belma <belmajkarim29@gmail.com>
2022-08-27 16:38:35 +0100

    Athentication UI

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import SplashScreen from "./src/screens/SplashScreen";
+import RootStackScreen from './src/screens/RootStackScreen';
 import SignInScreen from "./src/screens/SignInScreen";
+import {NavigationContainer} from "@react-navigation/native";
 import SignUpScreen from "./src/screens/SignUpScreen";
 
 export default function App() {
   console.log("changes saved");
 
   return (
-      <SafeAreaView style={styles.root}>
+    <NavigationContainer style={styles.root}>
       <SignUpScreen/>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </NavigationContainer>
+
   );
 }
 
